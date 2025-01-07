b5a8bff5a1ecf92e3ed5bfb3b27d376f94a562b8
Mariane-Medeiros <mariane.oli.med@gmail.com>
2025-01-06 22:16:29 -0300

    Failed attempt

--- teste.py
+++ teste.py
@@ -14,21 +14,40 @@ bombs = int(TAMANHO * 0.2)
 # trocar para nome em ingles
 indices_aleatorios = random.sample(range(TAMANHO), bombs)
 
-print(indices_aleatorios)
-
-# posso estar indo par aum lado negativo, antes da posiçao 0 com i-11
-for i in indices_aleatorios:
-    temp = i-11
-    for j in range(3):
-        list[j + temp] += 1
+# for i in indices_aleatorios:
+#    temp = i-11
+#    for j in range(3):
+#        list[j + temp] += 1
 
 for i in indices_aleatorios:
-    temp = i+9
-    for j in range(3):
-        if j + temp > len(list):
-            pass
-        else:
-            list[j + temp] += 1
+    anterior = i-11
+    posterior = i+9
+    loops = 3
+
+    bomba_inicio = True if i % 10 == 0 else False
+
+    bomba_final = True if (i + 1) % 10 == 0 else False
+    # checar como ele esta lidando com o primeiro e ultimo numero
+
+    if bomba_inicio:
+        anterior = i - 10
+        posterior = i + 10
+    else:
+        if (i-1) > 0:
+            list[i-1] += 1
+
+    if bomba_final:
+        loops = 2
+    else:
+        if (i+1) > len(list) - 1:
+            list[i+1] += 1
+
+    for j in range(loops):
+        if not (j + posterior > len(list) - 1 or j + posterior > len(list) - 1):
+            list[j + posterior] += 1
+            if (j + anterior) > 0:
+                list[j + anterior] += 1
+
 
 for i in indices_aleatorios:
     list[i] = 'B'
