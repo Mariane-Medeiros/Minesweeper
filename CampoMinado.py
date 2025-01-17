456201a70abfa2f5f0dd388148d7fdcae8687951
Mariane-Medeiros <mariane.oli.med@gmail.com>
2025-01-16 23:06:27 -0300

    Reveal numbers

--- CampoMinado.py
+++ CampoMinado.py
@@ -15,31 +15,12 @@ ROW, COLUMN = 10, 10
 cor_retangulo = (190, 190, 190)
 cor_linha = (255, 255, 255)
 fonte = pygame.font.SysFont('Arial', 30)
+# Variáveis globais
 ultimo_retangulo = []
-
-
-clock = pygame.time.Clock()
-windown = pygame.display.set_mode((WIDTH, HEIGHT))
-
-pygame.display.set_caption('Campo Minado')
-
-
-class field:
-    # MELHORAR ESSA CLASSE COM SELF
-    randombombs = np.random.choice([0, 9], size=100, p=[0.8, 0.2])
-    matriz = randombombs.reshape(10, 10)
-
-    for i in range(matriz.shape[0]):  # Linha
-        for j in range(matriz.shape[1]):  # Coluna
-            if matriz[i][j] >= 9:
-                for l in range(8):
-                    linha = i + ADJACENTE[l][0]
-                    coluna = j + ADJACENTE[l][1]
-                    if linha < 0 or coluna < 0 or linha > 9 or coluna > 9:
-                        pass
-                    else:
-                        matriz[linha][coluna] += 1
-
+numeros = []
+x_rect = 0
+y_rect = 0
+contador = 0
 
 largura_retangulo = 35
 altura_retangulo = 35
@@ -56,8 +37,37 @@ matriz_y_meio = matriz_y_inicial + (altura_total_matriz // 2)
 ROXO = (128, 0, 128)
 
 
+clock = pygame.time.Clock()
+windown = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption('Campo Minado')
+
+
+class Field:
+    def __init__(self):
+        randombombs = np.random.choice([0, 9], size=100, p=[0.8, 0.2])
+        self.matriz = randombombs.reshape(10, 10)
+
+    def create_matrix(self):
+        for i in range(self.matriz.shape[0]):  # Linha
+            for j in range(self.matriz.shape[1]):  # Coluna
+                if self.matriz[i][j] >= 9:
+                    for l in range(8):
+                        linha = i + ADJACENTE[l][0]
+                        coluna = j + ADJACENTE[l][1]
+                        if linha < 0 or coluna < 0 or linha > 9 or coluna > 9:
+                            pass
+                        else:
+                            self.matriz[linha][coluna] += 1
+        return self.matriz
+
+
+field = Field()
+matriz = field.create_matrix()
+
+
 def desenhar_tela():
-    # print(ultimo_retangulo)
+    global contador
     for i in range(ROW):
         for j in range(COLUMN):
             x = matriz_x_inicial + j * largura_retangulo
@@ -73,10 +83,18 @@ def desenhar_tela():
                 windown, ROXO, (x, y, largura_retangulo, altura_retangulo))
             pygame.display.update(
                 (x, y, largura_retangulo, altura_retangulo))
+    if numeros:
+        for i, numero in enumerate(numeros):
+            if i < len(ultimo_retangulo):
+                x, y = ultimo_retangulo[i]
+                # Renderizar o número como string
+                texto = fonte.render(str(numero), True, (255, 255, 255))
+                windown.blit(texto, (x, y))
 
 
 def find_tile(x, y):
-    global ultimo_retangulo
+    global x_rect, y_rect  # pra poder acessar a posiçao que vamos desenhar os numeros
+    global ultimo_retangulo  # so comeca a desenhar os retangulos quando tiver algum
     if x < matriz_x_inicial or x > matriz_x_final or y < matriz_y_inicial or y > matriz_y_final:  # nao sei se o uso de or esta certo
         return
     x -= matriz_x_inicial
@@ -87,14 +105,19 @@ def find_tile(x, y):
     x_rect = rec_posicao_linha * largura_retangulo + matriz_x_inicial
     y_rect = rec_posicao_coluna * altura_retangulo + matriz_y_inicial
     ultimo_retangulo.append((x_rect, y_rect))
-    # reveal_tile(x_rect, y_rect)
+    reveal_tile(rec_posicao_linha, rec_posicao_coluna)
 
 
-def reveal_tile(x, y):
-    pass
+def reveal_tile(l, c):
+    global contador
+    global numeros
+    numero = field.matriz[l][c]
+    numeros.append(numero)
+    # print(numeros)
 
 
 def main():
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
