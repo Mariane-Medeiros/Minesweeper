e88580dcfcaee2e43fa8c3059127ee56dccb1635
Mariane-Medeiros <mariane.oli.med@gmail.com>
2025-01-27 22:18:01 -0300

    Starting remove flag function

--- CampoMinado.py
+++ CampoMinado.py
@@ -19,15 +19,15 @@ cor_retangulo = (190, 190, 190)
 cor_linha = (255, 255, 255)
 fonte = pygame.font.SysFont('Arial', 30)
 positions_rec_left_click = []
-positions_rec_right_click = []
+positions_flag_right_click = []
 position_numbers = []
-array_draw_flag = []
+dic_flag = {}
 BLACK = (0, 0, 0)
 largura_retangulo = 35
 altura_retangulo = 35
 largura_total_matriz = COLUMN * largura_retangulo
 altura_total_matriz = ROW * altura_retangulo
-rec_posicao_linha, rec_posicao_coluna = 0, 0
+rec_indice_linha, rec_indice_coluna = 0, 0
 bomb_img = pygame.image.load("assets/bomb.png")
 flag_img = pygame.image.load("assets/flag.png")
 # Posição inicial para centralizar a matriz
@@ -90,14 +90,14 @@ state_matriz_propagation = np.zeros((ROW, COLUMN), dtype=int)
 class Reveal_empty:
     def control_progation():
         # linha e coluna depende de quem vai chamar a matriz
-        if field.matriz[rec_posicao_linha][rec_posicao_coluna] != 0:
+        if field.matriz[rec_indice_linha][rec_indice_coluna] != 0:
             return
         else:
             # coloco ele pra o array nao iniciar vazio
-            if state_matriz_propagation[rec_posicao_linha][rec_posicao_coluna] == 0:
+            if state_matriz_propagation[rec_indice_linha][rec_indice_coluna] == 0:
                 propagate_empty_array.append(
-                    (rec_posicao_linha, rec_posicao_coluna))
-                state_matriz_propagation[rec_posicao_linha][rec_posicao_coluna] = 1
+                    (rec_indice_linha, rec_indice_coluna))
+                state_matriz_propagation[rec_indice_linha][rec_indice_coluna] = 1
             Reveal_empty.propagate_empty_cells()
 
     # PRECISO ADICIONAR IGNORAR DIAGONAIS
@@ -119,12 +119,19 @@ class Reveal_empty:
                         state_matriz_propagation[linha][coluna] = 1
 
 
-def put_flag(x_absolute, y_absolute):
-    global array_draw_flag
-    # find_tile(x_absolute, y_absolute)
-    if state_matriz_rec[rec_posicao_linha][rec_posicao_coluna] == 0:
-        array_draw_flag.append((rec_posicao_linha, rec_posicao_coluna))
-        state_matriz_rec[rec_posicao_linha][rec_posicao_coluna] = 1
+def add_flag(x, y):
+    global dic_flag
+    if state_matriz_rec[rec_indice_linha][rec_indice_coluna] == 0:
+        dic_flag[rec_indice_linha, rec_indice_coluna] = (x, y)
+        state_matriz_rec[rec_indice_linha][rec_indice_coluna] = 1
+        state_matriz_number[rec_indice_linha][rec_indice_coluna] = 1
+        state_matriz_propagation[rec_indice_linha][rec_indice_coluna] = 1
+
+
+'''def remove_flag(linha, coluna):
+    if state_matriz_rec[rec_indice_linha][rec_indice_coluna] == 1:
+        if (linha, coluna) in dic_flag:
+            dic_flag.pop((linha, coluna))'''
 
 
 def draw_matrix():
@@ -165,16 +172,19 @@ def draw_number():
 
 
 def draw_flag():
-    if array_draw_flag:
-        for i in range(len(array_draw_flag)):
-            if i < len(array_draw_flag):
-                x, y = positions_rec_right_click[i]
+    if bool(dic_flag) == True:
+        print('test')
+        for i in range(len(dic_flag)):
+
+            if i < len(positions_flag_right_click):
+                x, y = positions_flag_right_click[i]
+                # print(dic_flag, positions_flag_right_click)
                 windown.blit(flag_img, (x, y))
 
 
 def find_tile(x, y):
     # pra poder acessar a posiçao que vamos desenhar os numeros
-    global rec_posicao_linha, rec_posicao_coluna
+    global rec_indice_linha, rec_indice_coluna
     # saber se clicou fora da matriz
     if x < matriz_x_inicial or x > matriz_x_final or y < matriz_y_inicial or y > matriz_y_final:
         return
@@ -182,12 +192,12 @@ def find_tile(x, y):
     x -= matriz_x_inicial
     y -= matriz_y_inicial
     # divido o valor de x e y pelos retangulos para saber a linha e coluna
-    rec_posicao_coluna = x // largura_retangulo
-    rec_posicao_linha = y // altura_retangulo
+    rec_indice_coluna = x // largura_retangulo
+    rec_indice_linha = y // altura_retangulo
 
 
 def rect_drawing_coordinates(l, c, array):
-    global positions_rec_left_click, positions_rec_right_click
+    global positions_rec_left_click, positions_flag_right_click
     x_rect = c * largura_retangulo + matriz_x_inicial
     y_rect = l * altura_retangulo + matriz_y_inicial
     if state_matriz_number[l][c] == 0:
@@ -221,14 +231,15 @@ def main():
             if event.button == 1:
                 find_tile(x, y)
                 rect_drawing_coordinates(
-                    rec_posicao_linha, rec_posicao_coluna, positions_rec_left_click)
-                reveal_numbers(rec_posicao_linha, rec_posicao_coluna)
+                    rec_indice_linha, rec_indice_coluna, positions_rec_left_click)
+                reveal_numbers(rec_indice_linha, rec_indice_coluna)
                 Reveal_empty.control_progation()
             if event.button == 3:
                 find_tile(x, y)
                 rect_drawing_coordinates(
-                    rec_posicao_linha, rec_posicao_coluna, positions_rec_right_click)
-                put_flag(x, y)
+                    rec_indice_linha, rec_indice_coluna, positions_flag_right_click)
+                add_flag(x, y)
+                # remove_flag(rec_indice_linha, rec_indice_coluna)
 
 
 if __name__ == '__main__':
